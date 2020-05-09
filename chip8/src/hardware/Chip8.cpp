d849732921e547e878e80865f3c9514af924ed77
HaruGakkaP <ffdd270@gmail.com>
2020-05-10 00:48:51 +0900

    Add Comment.

--- chip8/src/hardware/Chip8.cpp
+++ chip8/src/hardware/Chip8.cpp
@@ -4,7 +4,18 @@
 
 #include "Chip8.h"
 
+
 Chip8::Chip8()
 {
+
+}
+
+void Chip8::CPUReset()
+{
+	mAddressI = 0;
+	mProgramCounter = 0x200; // 왜 0x200이징;
+	memset(mRegisters, 0, sizeof(mRegisters));
+
+
 
 }
--- chip8/src/hardware/Chip8.h
+++ chip8/src/hardware/Chip8.h
@@ -14,6 +14,20 @@ public:
 	Chip8();
 
 private:
+	void CPUReset();
+
+
+	// CHIP 8 내부 메모리 & CPU등 설정.
+
+/* CHIP 8 기기는 아래로 이루어져있다.
+ *
+    * Memory of 0xFFF bytes ( 0xFFF 바이트짜리 메모리. )
+    * 16 8-Bit data registers, called V0 to VF. VF doubles as the carry flag ( 16개의 8비트 데이터 레지스터, V0~VF라고 불린다. )
+    * 16 Bit address register I which is used to access memory ( 메모리에 접근할 때 사용하는 16비트 주소 레지스터. )
+    * 16 Bit program counter and a stack ( 16 비트 프로그램 카운터 ( 코드 실행 위치를 기억하는 장치 )와 스택. )
+ *
+ */
+private:
 	BYTE mGameMemory[0xFFF]; // 메모리 : 0xFFF 바이트.
 	BYTE mRegisters[16]; // 16개의 레지스터들.
 	WORD mAddressI; // 레지스터 I의 16비트 주소.
