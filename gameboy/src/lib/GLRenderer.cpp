297d8917c198e5948368b73e467954bdd2e3e9c5
HaruGakka <ffdd270@gmail.com>
2020-05-29 13:48:07 +0900

    OpenGL Renderer 기본 구현. GLFW 바인딩.

---
+++ gameboy/src/lib/GLRenderer.cpp
@@ -0,0 +1,15 @@
+//
+// Created by HaruGakkaP on 2020-05-29.
+//
+
+#include "GLRenderer.h"
+
+#include <gl/gl3w.h>
+
+using namespace Angelica;
+
+void GLRenderer::init()
+{
+	gl3wInit();
+
+}
---
+++ gameboy/src/lib/GLRenderer.h
@@ -0,0 +1,21 @@
+//
+// Created by HaruGakkaP on 2020-05-29.
+//
+
+#ifndef GAMEBOY_GLRENDERER_H
+#define GAMEBOY_GLRENDERER_H
+
+
+
+namespace Angelica
+{
+	class GLRenderer
+	{
+	public:
+		void init();
+	};
+
+}
+
+
+#endif //GAMEBOY_GLRENDERER_H
