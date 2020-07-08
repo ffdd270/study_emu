d90b08319f48c6e0d8a01d6528ed6f2769767286
HaruGakkaP <ffdd270@gmail.com>
2020-07-08 23:34:45 +0900

    Add Lua Binding...

---
+++ gameboy/src/lua-binding/gameboy_luabinding.cpp
@@ -0,0 +1,4 @@
+//
+// Created by HaruGakkaP on 2020-07-08.
+//
+
---
+++ gameboy/src/lua-binding/gameboy_luabinding.h
@@ -0,0 +1,8 @@
+//
+// Created by HaruGakkaP on 2020-07-08.
+//
+
+#ifndef GAMEBOY_GAMEBOY_LUABINDING_H
+#define GAMEBOY_GAMEBOY_LUABINDING_H
+
+#endif //GAMEBOY_GAMEBOY_LUABINDING_H
