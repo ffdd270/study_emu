0a739c4406e75359246b08ae8ca801fb004231f6
ffdd270 <ffdd270@gmail.com>
2020-10-12 00:12:22 +0900

    bind SFML!

--- gameboy/src/main.cpp
+++ gameboy/src/main.cpp
@@ -1,48 +1,40 @@
-#include "angelica/GLRenderer.h"
-#include "angelica/Window.h"
-#include "angelica/typedef.h"
-#include "gameboy/GameboyCPU.h"
-#include <iostream>
+#include "imgui.h"
+#include "imgui-SFML.h"
 
+
+#include <SFML/Graphics.hpp>
+#include <SFML/OpenGL.hpp>
 
 int main()
 {
+	sf::RenderWindow window(sf::VideoMode(640, 480), "Gameboy");
+	window.setFramerateLimit(60);
+	ImGui::SFML::Init(window);
 
-	Angelica::Window window;
-	Angelica::GLRenderer renderer;
-
-	window.create( "I Will Keep Comming", 640, 320 );
-	renderer.init();
-
-
-	const float ortho_projection[4][4] =
-			{
-					{ 2.0f/ 640, 0.0f, 0.0f, 0.0f },
-					{ 0.0f, 2.0f / -320, 0.0f, 0.0f },
-					{ 0.0f, 0.0f, -1.0f, 0.0f },
-					{-1.0f, 1.0f,  0.0f, 1.0f },
-			};
-
-	Angelica::Matrix4x4 mat( ortho_projection );
-	Angelica::GLProgram program = renderer.getDefaultProgram();
-	program.addAttrib( "position");
-	program.addUniform("proj_ortho");
-
-	static const GLfloat black[] = { 0.0f, 0.0f, 0.0f, 1.0f };
-
-	while( !window.windowShouldClose() )
+	sf::Clock deltaClock;
+	while (window.isOpen())
 	{
-		window.pollEvents();
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			ImGui::SFML::ProcessEvent(event);
 
-		glClearBufferfv(GL_COLOR, 0, black);
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
 
-		program.useProgram();
-		glPointSize( 10.0f );
+		ImGui::SFML::Update(window, deltaClock.restart());
 
-		program.setUniformMatrix4x4( "proj_ortho", mat );
-		program.setVertexAttrib2f( "position", {  320, 160  } );
-		glDrawArrays( GL_POINTS, 0, 1 );
+		ImGui::Begin("Hello, world!");
+		ImGui::Button("Look at this pretty button");
+		ImGui::End();
 
-		window.swapBuffers();
+		window.clear();
+		ImGui::SFML::Render(window);
+		window.display();
 	}
+
+	ImGui::SFML::Shutdown();
+
+	return 0;
 }
