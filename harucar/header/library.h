27f7c7a87cb6da18db2a77608a4aa8445c392d06
ffdd270 <ffdd270@gmail.com>
2020-10-11 23:35:24 +0900

    Init Harucar Project.

---
+++ harucar/header/library.h
@@ -0,0 +1,6 @@
+#ifndef HARUCAR_LIBRARY_H
+#define HARUCAR_LIBRARY_H
+
+void hello();
+
+#endif //HARUCAR_LIBRARY_H
---
+++ harucar/src/library.cpp
@@ -0,0 +1,8 @@
+#include "../header/library.h"
+
+#include <iostream>
+
+void hello()
+{
+	std::cout << "Hello, World!" << std::endl;
+}
