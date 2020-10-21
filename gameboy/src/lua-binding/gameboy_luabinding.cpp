fae38d6c38bb000e037542f0dedb8888c3379040
HaruGakkaP <ffdd270@gmail.com>
2020-10-21 13:58:57 +0900

    Binding Lua.

--- gameboy/src/lua-binding/gameboy_luabinding.cpp
+++ gameboy/src/lua-binding/gameboy_luabinding.cpp
@@ -2,3 +2,11 @@
 // Created by HaruGakkaP on 2020-07-08.
 //
 
+#include "gameboy_luabinding.h"
+#include <LuaBridge.h>
+
+void gameboy_lua_binding(lua_State *lua_state)
+{
+	
+}
+
--- gameboy/src/lua-binding/gameboy_luabinding.h
+++ gameboy/src/lua-binding/gameboy_luabinding.h
@@ -5,4 +5,9 @@
 #ifndef GAMEBOY_GAMEBOY_LUABINDING_H
 #define GAMEBOY_GAMEBOY_LUABINDING_H
 
+#include <lua.hpp>
+
+void gameboy_lua_binding( lua_State * lua_state );
+
+
 #endif //GAMEBOY_GAMEBOY_LUABINDING_H
