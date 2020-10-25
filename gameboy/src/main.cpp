ede36aa867517efce8c9e5659bc14ca73e42393a
ffdd270 <ffdd270@gmail.com>
2020-10-26 00:49:57 +0900

    Add basic injection logic.

--- gameboy/src/main.cpp
+++ gameboy/src/main.cpp
@@ -1,19 +1,30 @@
+#include <iostream>
+
 #include "imgui.h"
 #include "imgui-SFML.h"
 
 #include <SFML/Graphics.hpp>
 #include <SFML/OpenGL.hpp>
 
+#include "util/util_string.h"
 #include "gameboy/GameboyCPU.h"
 #include "gameboy/GameboyCPUBroker.h"
 #include "cpu/cpu_viewer.h"
+
+
 
 int main()
 {
 	GameboyCPU cpu;
 	GameboyCPUBroker broker;
+
+	std::shared_ptr<UIEventProtocol> protocol_ptr = std::make_shared<UIEventProtocol>();
 	std::shared_ptr<CPUProvider> provider_ptr = broker.MakeProvider( cpu );
+	std::shared_ptr<Structure::InputBuffer> input_buffer_ptr = std::make_shared<Structure::InputBuffer>( 300 );
+
 	CPUViewer viewer;
+
+	viewer.SetInputBuffer( input_buffer_ptr );
 
 	cpu.InjectionMemory( 0b00111110 ); // LD A, imm8
 	cpu.InjectionMemory( 0x0 );
@@ -49,7 +60,39 @@ int main()
 		ImGui::Button("Look at this pretty button");
 		ImGui::End();
 
-		viewer.Render( provider_ptr );
+		viewer.Render( provider_ptr, protocol_ptr );
+
+		if ( UIEventHelperFunction::FireEvent( *protocol_ptr, "Injection" ) )
+		{
+			std::vector<std::string> parse_result = Util::string_split( input_buffer_ptr->GetViewString() );
+
+			try
+			{
+				std::vector<BYTE> instructions;
+				instructions.reserve( parse_result.size() );
+
+				for(const auto & i : parse_result)
+				{
+					instructions.emplace_back( std::stoi( i, nullptr, 16 ) );
+				}
+
+				for(const BYTE & value : instructions )
+				{
+					cpu.InjectionMemory( value );
+				}
+			}
+			catch(std::invalid_argument & invalid_argument)
+			{
+				std::cout << "WRONG INPUT" << std::endl;
+			}
+
+			broker.UpdateProvider( cpu, provider_ptr );
+		}
+
+		if ( UIEventHelperFunction::FireEvent( *protocol_ptr, "Next Step") )
+		{
+			cpu.NextStep();
+		}
 
 		window.clear();
 		ImGui::SFML::Render(window);
