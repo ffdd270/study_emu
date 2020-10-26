3a9563b5f510ac232fe20996bd5d17635243da06
ffdd270 <ffdd270@gmail.com>
2020-10-27 00:46:17 +0900

    Add Logger

--- harucar/include/common/common_logger.h
+++ harucar/include/common/common_logger.h
@@ -10,7 +10,7 @@
 
 #include <vector>
 
-namespace HaruCar::Common::System
+namespace HaruCar::Common::Log
 {
 	enum class LogLevels
 	{
@@ -25,18 +25,38 @@ namespace HaruCar::Common::System
 		LogLevels info;
 	};
 
+	class LoggerEnd;
 
 	class Logger
 	{
 	public:
+		friend class LoggerEnd;
 		void Log( LogLevels level, std::string_view str );
 
 		void LogWarn( std::string_view str ) { Log(  LogLevels::WARNING, str ); }
 		void LogError( std::string_view str ) { Log(  LogLevels::ERROR, str ); }
 		void LogInfo( std::string_view str ) { Log(  LogLevels::INFO, str ); }
+
+		Logger & operator<<( const std::string_view & str );
+
+		[[nodiscard]] const LogData & GetData( size_t index ) const;
+		[[nodiscard]] const std::vector<LogData> & GetDatas() const;
+		[[nodiscard]] size_t GetSize() const { return mLogData.size(); }
 	private:
+		void logStreamEnd();
+
+		std::string mStream;
 		std::vector<LogData> mLogData;
 	};
+
+	class LoggerEnd
+	{
+	public:
+		void operator=(Logger & other);
+	};
+
+#define LOG( Logger )\
+	LoggerEnd() = Logger
 }
 
 #endif //HARUCAR_COMMON_LOGGER_H
