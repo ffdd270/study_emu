19c9c2ba6607f77da99ebfa8e405ae6a539e752b
HaruGakkaP <ffdd270@gmail.com>
2020-11-04 13:42:31 +0900

    [WIP] Lua ImGui Handling

---
+++ gameboy/src/lua-binding/LuaImGuiHandler.h
@@ -0,0 +1,16 @@
+#ifndef GAMEBOY_LUAIMGUIHANDLER_H
+#define GAMEBOY_LUAIMGUIHANDLER_H
+
+
+class LuaImGuiHandler
+{
+public:
+	void MakeViewer(  );
+
+private:
+
+
+};
+
+
+#endif //GAMEBOY_LUAIMGUIHANDLER_H
