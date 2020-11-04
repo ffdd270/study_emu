d1d164772b0474790b582bb4eb2a246e4eb9a21e
ffdd270 <ffdd270@gmail.com>
2020-11-04 23:59:10 +0900

    [WIP] Push to luaL_ref

--- gameboy/src/lua-binding/LuaContext.cpp
+++ gameboy/src/lua-binding/LuaContext.cpp
@@ -23,6 +23,16 @@ bool LuaContext::ExecuteFunction(std::string_view func_name)
 	return ( lua_pcall( mLuaState, 0, 0, 0 ) == 0 );
 }
 
+bool LuaContext::ExecuteRefFunction(LuaContextRefId ref_id)
+{
+	if ( !IsCorrectKey( ref_id ) )
+	{
+		return false;
+	}
+
+	return false;
+}
+
 bool LuaContext::ExecuteString(std::string_view execute_string)
 {
 	if ( luaL_dostring( mLuaState, execute_string.data() ) ) // Return 있으면 문제가..
@@ -44,23 +54,7 @@ bool LuaContext::ExecuteFile(std::string_view file_path )
 	return true;
 }
 
-void LuaContext::init()
-{
-	mRefCnt = 0;
-	mReloadCnt++;
-	mReferenceMap.clear();
 
-	mLuaState = luaL_newstate();
-	luaL_openlibs( mLuaState );
-
-	gameboy_lua_binding( mLuaState );
-}
-
-std::string_view LuaContext::GetLastError()
-{
-	const char * last_err = lua_tostring(mLuaState, -1);
-	return last_err == nullptr ?  "" : last_err ;
-}
 
 bool LuaContext::IsExistGlobalValue(std::string_view value_name)
 {
@@ -70,10 +64,35 @@ bool LuaContext::IsExistGlobalValue(std::string_view value_name)
 	return !rtn;
 }
 
-
-LuaContextRefId LuaContext::MakeLuaCallback(int stack)
+LuaContextRefId LuaContext::contextRefIdGenerate()
 {
+	uint64_t key = mReloadCnt & 0xffffu;
+	uint64_t unique_id = mRefCnt & 0x0000ffffffffu;
 
+	// 32비트 점프.
+	uint64_t ref_id = ( key << 4u * 8u ) | unique_id;
+
+	mRefCnt++;
+
+	return ref_id;
+}
+
+bool LuaContext::IsCorrectKey(LuaContextRefId ref_id) const
+{
+	uint32_t key = ref_id & 0xffff00000000u; //상위 16비트
+	uint32_t expect_key = mReloadCnt & 0xffffu;
+
+	return key == expect_key;
+}
+
+LuaContextRefId LuaContext::MakeLuaCallback()
+{
+	LuaRefId ref_id = luaL_ref( mLuaState, LUA_REGISTRYINDEX );
+	LuaContextRefId context_ref_id = contextRefIdGenerate();
+
+	mReferenceMap.insert( std::make_pair( context_ref_id, ref_id ) );
+
+	return context_ref_id;
 }
 
 void LuaContext::Reload()
@@ -83,27 +102,31 @@ void LuaContext::Reload()
 	init();
 }
 
+std::string LuaContext::GetLastError()
+{
+	if ( !mLastError.empty() )
+	{
+		std::string return_value = mLastError;
+		return mLastError;
+	}
+
+	const char * last_err = lua_tostring(mLuaState, -1);
+	return last_err == nullptr ?  "" : last_err ;
+}
+
+void LuaContext::init()
+{
+	mRefCnt = 0;
+	mReloadCnt++;
+	mReferenceMap.clear();
+
+	mLuaState = luaL_newstate();
+	luaL_openlibs(mLuaState);
+
+	gameboy_lua_binding(mLuaState);
+}
 
 LuaContext::~LuaContext()
 {
 	lua_close( mLuaState );
-}
-
-LuaContextRefId LuaContext::contextRefIdGenerate() const
-{
-	uint64_t key = mReloadCnt & 0xffffu;
-	uint64_t unique_id = mRefCnt & 0x0000ffffffffu;
-
-	// 32비트 점프.
-	uint64_t ref_id = ( key << 4u * 8u ) | unique_id;
-
-	return ref_id;
-}
-
-bool LuaContext::isCurrentKey(LuaContextRefId ref_id) const
-{
-	uint32_t key = ref_id & 0xffff00000000u; //상위 16비트
-	uint32_t expect_key = mReloadCnt & 0xffffu;
-
-	return key == expect_key;
 }
--- gameboy/src/lua-binding/LuaContext.h
+++ gameboy/src/lua-binding/LuaContext.h
@@ -20,33 +20,37 @@ public:
 	LuaContext();
 
 	bool ExecuteFunction( std::string_view func_name );
+	bool ExecuteRefFunction( LuaContextRefId ref_id );
+
 	bool ExecuteString( std::string_view execute_string );
 	bool ExecuteFile( std::string_view file_path );
 
 	bool IsExistGlobalValue( std::string_view value_name );
 
-	// Need..
-	// lua_stack[ stack ] == lua_function
-	LuaContextRefId MakeLuaCallback( int stack );
-
-	void Reload();
-
-	std::string_view GetLastError();
-
-	~LuaContext();
-private:
 	// 48비트 키 제너레이트.
 	// 초반 ffff, => reload cnt.
 	// 나머지,
 	// reload cnt를 높일까, 아니면 key를 재활용할까.. 고민중.
-	[[nodiscard]] LuaContextRefId contextRefIdGenerate() const;
-	[[nodiscard]] bool isCurrentKey( LuaContextRefId ref_id ) const;
+	[[nodiscard]] bool IsCorrectKey(LuaContextRefId ref_id ) const;
+
+	// On After Push
+	LuaContextRefId MakeLuaCallback();
+
+	void Reload();
+
+	std::string GetLastError();
+
+	~LuaContext();
+private:
+	[[nodiscard]] LuaContextRefId contextRefIdGenerate();
 	void init();
 private:
 	std::unordered_map<LuaContextRefId, LuaRefId> mReferenceMap;
+	std::string mLastError;
 
 	size_t mReloadCnt = 0;
 	size_t mRefCnt = 0;
+
 	lua_State * mLuaState = nullptr;
 };
 
