bdce5d5e1c1fa870b79b68c5727a332a6bcc50cd
HaruGakkaP <ffdd270@gmail.com>
2020-12-02 14:01:44 +0900

    Refactroing : Setter is Section.

--- gameboy/test/src/mbc1.cpp
+++ gameboy/test/src/mbc1.cpp
@@ -16,6 +16,127 @@ void MBC1GetterTest( MBC1 & mbc1 )
 	}
 }
 
+void MBC1SetterTest( MBC1 & mbc1 )
+{
+
+	SECTION( "Set Ram Enabled. (0x0000~0x1fff)" )
+	{
+		WHEN("write to 0x02ff, 0xa.")
+		{
+			mbc1.Set( 0x02ff, 0xA );
+
+			THEN("RAM is Enabled!")
+			{
+				REQUIRE( mbc1.IsRAMActive() );
+			}
+		}
+
+		WHEN("write to 0x03ff, 0x0.")
+		{
+			mbc1.Set( 0x03ff, 0x0 );
+
+			THEN("RAM is disabled.")
+			{
+				REQUIRE_FALSE( mbc1.IsRAMActive() );
+			}
+		}
+
+		WHEN("write to 0x14ff, 0x0.")
+		{
+			mbc1.Set( 0x02ff, 0xA );
+
+			THEN("RAM is Enabled.")
+			{
+				REQUIRE( mbc1.IsRAMActive() );
+			}
+		}
+	}
+
+	SECTION("Set Rom Bank Least 5Bit.  (0x2000~0x3fff)")
+	{
+		WHEN("Write to 0x2000, 0b10111111")
+		{
+			BYTE set_value = 0b10111111u;
+			BYTE expect_value = set_value & 0b00011111u; // 5Bit 빼고 제거.
+
+			mbc1.Set( 0x2000, set_value );
+
+			THEN("ROM BANK same as expect value.")
+			{
+				REQUIRE( mbc1.GetRomBankNumber() == expect_value );
+			}
+		}
+
+		WHEN( "Write to 0x32f3, 0b0.")
+		{
+			mbc1.Set( 0x2000, 0 );
+			THEN("ROM BANK is 1!")
+			{
+				REQUIRE(mbc1.GetRomBankNumber() == 1);
+			}
+		}
+	}
+
+	SECTION("Set Rom Bank Hi 2Bit. (0x4000~0x5fff)")
+	{
+		WHEN("Write to 0x5000, 0x0f.")
+		{
+			BYTE set_value = 0x0f;
+			BYTE expect_value = ( set_value & 0x03u ) << 5u;
+			mbc1.Set( 0x5000, set_value );
+
+			THEN("ROM BANK : ( 0x0f & 0x03 ) << 5u")
+			{
+				// 기본 값 + .
+				REQUIRE( mbc1.GetRomBankNumber() == ( expect_value + 1 ) );
+			}
+		}
+	}
+
+	SECTION("Set BankMode. (0x6000~0x7fff)")
+	{
+		REQUIRE( mbc1.GetBankMode() == BankMode::ROM ); // 기본 값은 ROM.
+
+		WHEN("Write to 0x7000, 0x1")
+		{
+			mbc1.Set( 0x7000, 0x1 );
+			THEN("BANK MODE : RAM")
+			{
+				REQUIRE( mbc1.GetBankMode() == BankMode::RAM );
+			}
+		}
+
+		WHEN("Write to 0x7100, 0x0")
+		{
+			mbc1.Set( 0x7100, 0x0 );
+			THEN("BANK MODE : ROM")
+			{
+				REQUIRE( mbc1.GetBankMode() == BankMode::ROM );
+			}
+		}
+
+		WHEN("Write to 0x6000, 0x2 ")
+		{
+			THEN("THROW!")
+			{
+				REQUIRE_THROWS( mbc1.Set(0x6000, 0x2) );
+			}
+		}
+	}
+
+	WHEN("Low => 0b11010101 ( SET 10101 ), Hi => 0b11111111 ( SET 0b11 ) ")
+	{
+		mbc1.Set(0x22ff, 0b11010101);
+		mbc1.Set(0x5500, 0xff);
+
+		THEN(" Result => 0b1110101")
+		{
+			REQUIRE(mbc1.GetRomBankNumber() == 0b1110101);
+		}
+	}
+
+}
+
 
 SCENARIO("Use MBC1.", "[MBC]")
 {
@@ -27,120 +148,9 @@ SCENARIO("Use MBC1.", "[MBC]")
 
 		MBC1 mbc1( std::move(cart) );
 
-		SECTION( "Set Ram Enabled. (0x0000~0x1fff)" )
+		SECTION("Setter")
 		{
-			WHEN("write to 0x02ff, 0xa.")
-			{
-				mbc1.Set( 0x02ff, 0xA );
-
-				THEN("RAM is Enabled!")
-				{
-					REQUIRE( mbc1.IsRAMActive() );
-				}
-			}
-
-			WHEN("write to 0x03ff, 0x0.")
-			{
-				mbc1.Set( 0x03ff, 0x0 );
-
-				THEN("RAM is disabled.")
-				{
-					REQUIRE_FALSE( mbc1.IsRAMActive() );
-				}
-			}
-
-			WHEN("write to 0x14ff, 0x0.")
-			{
-				mbc1.Set( 0x02ff, 0xA );
-
-				THEN("RAM is Enabled.")
-				{
-					REQUIRE( mbc1.IsRAMActive() );
-				}
-			}
-		}
-
-		SECTION("Set Rom Bank Least 5Bit.  (0x2000~0x3fff)")
-		{
-			WHEN("Write to 0x2000, 0b10111111")
-			{
-				BYTE set_value = 0b10111111u;
-				BYTE expect_value = set_value & 0b00011111u; // 5Bit 빼고 제거.
-
-				mbc1.Set( 0x2000, set_value );
-
-				THEN("ROM BANK same as expect value.")
-				{
-					REQUIRE( mbc1.GetRomBankNumber() == expect_value );
-				}
-			}
-
-			WHEN( "Write to 0x32f3, 0b0.")
-			{
-				mbc1.Set( 0x2000, 0 );
-				THEN("ROM BANK is 1!")
-				{
-					REQUIRE(mbc1.GetRomBankNumber() == 1);
-				}
-			}
-		}
-
-		SECTION("Set Rom Bank Hi 2Bit. (0x4000~0x5fff)")
-		{
-			WHEN("Write to 0x5000, 0x0f.")
-			{
-				BYTE set_value = 0x0f;
-				BYTE expect_value = ( set_value & 0x03u ) << 5u;
-				mbc1.Set( 0x5000, set_value );
-
-				THEN("ROM BANK : ( 0x0f & 0x03 ) << 5u")
-				{
-					// 기본 값 + .
-					REQUIRE( mbc1.GetRomBankNumber() == ( expect_value + 1 ) );
-				}
-			}
-		}
-
-		SECTION("Set BankMode. (0x6000~0x7fff)")
-		{
-			REQUIRE( mbc1.GetBankMode() == BankMode::ROM ); // 기본 값은 ROM.
-
-			WHEN("Write to 0x7000, 0x1")
-			{
-				mbc1.Set( 0x7000, 0x1 );
-				THEN("BANK MODE : RAM")
-				{
-					REQUIRE( mbc1.GetBankMode() == BankMode::RAM );
-				}
-			}
-
-			WHEN("Write to 0x7100, 0x0")
-			{
-				mbc1.Set( 0x7100, 0x0 );
-				THEN("BANK MODE : ROM")
-				{
-					REQUIRE( mbc1.GetBankMode() == BankMode::ROM );
-				}
-			}
-
-			WHEN("Write to 0x6000, 0x2 ")
-			{
-				THEN("THROW!")
-				{
-					REQUIRE_THROWS( mbc1.Set(0x6000, 0x2) );
-				}
-			}
-		}
-
-		WHEN("Low => 0b11010101 ( SET 10101 ), Hi => 0b11111111 ( SET 0b11 ) ")
-		{
-			mbc1.Set(0x22ff, 0b11010101);
-			mbc1.Set(0x5500, 0xff);
-
-			THEN(" Result => 0b1110101")
-			{
-				REQUIRE(mbc1.GetRomBankNumber() == 0b1110101);
-			}
+			MBC1SetterTest( mbc1 );
 		}
 
 		SECTION("Getter")
