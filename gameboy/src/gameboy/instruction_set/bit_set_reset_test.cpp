1120ec4969919c5f7dce8dc4e1bb277e6bf1e343
ffdd270 <ffdd270@gmail.com>
2020-12-05 20:18:25 +0900

    Changed : 'bit set, reset, test', fix 8bit arguement.

--- gameboy/src/gameboy/instruction_set/bit_set_reset_test.cpp
+++ gameboy/src/gameboy/instruction_set/bit_set_reset_test.cpp
@@ -6,10 +6,11 @@
 
 void GameboyCPU::bitTest(BYTE op_code)
 {
+	BYTE param = op_code & 0b00000111u;
 	BYTE bit_position = ( op_code & 0b00111000u ) >> 3u;
-	BYTE reg_value = get8BitArgumentValue(  op_code & 0b00000111u );
+	BYTE argument_value = get8BitArgumentValue(param);
 
-	BYTE result = (reg_value & (0b1u << bit_position)) >> bit_position;
+	BYTE result = (argument_value & (0b1u << bit_position)) >> bit_position;
 
 	setFlagZ( result == 0 );
 	setFlagH( true );
@@ -19,15 +20,19 @@ void GameboyCPU::bitTest(BYTE op_code)
 void GameboyCPU::setBit(BYTE op_code)
 {
 	BYTE bit_position = ( op_code & 0b00111000u ) >> 3u;
-	BYTE & reg_value = get8BitArgumentValue(  op_code & 0b00000111u );
+	BYTE param = op_code & 0b00000111u;
+	BYTE argument_value = get8BitArgumentValue(param );
 
-	reg_value |= 0b1u << bit_position;
+	argument_value |= 0b1u << bit_position;
+	set8BitArgumentValue( param, argument_value );
 }
 
 void GameboyCPU::resetBit(BYTE op_code)
 {
 	BYTE bit_position = ( op_code & 0b00111000u ) >> 3u;
-	BYTE & reg_value = get8BitArgumentValue(  op_code & 0b00000111u );
+	BYTE param = op_code & 0b00000111u;
+	BYTE argument_value = get8BitArgumentValue( param );
 
-	reg_value &= ( 0xffu ^ ( 0b1u << bit_position ) ); // 다르면 1, 같으면 0. 해서.. 함.
+	argument_value &= (0xffu ^ ( 0b1u << bit_position ) ); // 다르면 1, 같으면 0. 해서.. 함.
+	set8BitArgumentValue( param, argument_value );
 }
