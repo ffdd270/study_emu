9b0fae43131670bf0e29dd64c67f75757fa79975
ffdd270 <ffdd270@gmail.com>
2021-01-16 00:04:39 +0900

    Impl : Draw GPU Pixels.

--- gameboy/src/gpu-playground/texture_test.cpp
+++ gameboy/src/gpu-playground/texture_test.cpp
@@ -17,29 +17,57 @@ public:
 
 	}
 
+	using Pixels = std::array< UINT8, GPUHelper::ScreenWidth * GPUHelper::ScreenHeight * 4 >;
+	using ScreenLine = std::array<GPUHelper::ColorPallet, GPUHelper::ScreenWidth>;
+
 	void render()
 	{
+		gpu.NextStep( 1 );
+
+
+		const ColorScreenBits * ptr_bits = gpu.GetScreenData();
+		const ColorScreenBits & ref_bits = (*ptr_bits);
+
+		// 이게 Draw 로직임.
+		for ( int y = 0; y < GPUHelper::ScreenHeight; y++ )
+		{
+			for( int x = 0; x < GPUHelper::ScreenWidth; x++ )
+			{
+				const GPUHelper::ColorPallet & ref_pallet = ref_bits[y][x];
+
+				// RGBA
+				// 0x1f를 -> 0xff로 사상. 8배 차이니 8 곱해줌.
+				// 4는? 4비트라서.
+				mPixels[ ( y * GPUHelper::ScreenWidth * 4 ) + ( x * 4 ) + 0 ] = ref_pallet.Red() * 8;
+				mPixels[ ( y * GPUHelper::ScreenWidth * 4 ) + ( x * 4 ) + 1 ] = ref_pallet.Blue() * 8;
+				mPixels[ ( y * GPUHelper::ScreenWidth * 4 ) + ( x * 4 ) + 2 ] = ref_pallet.Green() * 8;
+				mPixels[ ( y * GPUHelper::ScreenWidth * 4 ) + ( x * 4 ) + 3 ] = 255;
+			}
+		}
+
 		mTexture.update( mPixels.data() );
 	}
 
-	UINT32 getTexture()
+	const sf::Texture * getTexture() const
 	{
-		return mTexture.getNativeHandle();
+		return &mTexture;
 	}
 
 private:
-	std::array< UINT8, GPUHelper::ScreenWidth * GPUHelper::ScreenHeight * 4 > mPixels;
+	Pixels mPixels;
+	GPU gpu;
 	sf::Texture mTexture;
+	sf::Sprite mSprite;
 };
 
 
 TextureTest * ptr_texture_test = nullptr;
 
-void ImGui_Texture_Draw( UINT32 native_texture_handle )
+void ImGui_Texture_Draw( const sf::Texture * texture_handle )
 {
 	ImGui::Begin("Texture Test");
 
-	ImGui::Image( static_cast<ImTextureID>(&native_texture_handle), ImVec2( GPUHelper::ScreenWidth, GPUHelper::ScreenHeight ) );
+	ImGui::Image( *texture_handle );
 
 	ImGui::End();
 }
