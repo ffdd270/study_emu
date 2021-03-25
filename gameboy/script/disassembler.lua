ff8e1c4a4da758d04e4a84fa391c7b5b8604376d
ffdd270 <ffdd270@gmail.com>
2021-03-26 02:07:41 +0900

    GEP-27 Gameboy - Disasmbler In Runtime
    음수부 문제와, 지정된 범위로 뿌려줄 수 있는 기능 개발.

--- gameboy/script/disassembler.lua
+++ gameboy/script/disassembler.lua
@@ -10,6 +10,35 @@ function get_op_translate_value( memory, mem_pos, op )
 		return op, mem_pos + 2
 	end
 
+	if op == '(a16)' or op == '(d16)' then
+		local hi = memory:GetValue( mem_pos + 2 )
+		local lo = memory:GetValue( mem_pos + 1 )
+
+		op = to_hex_string(  hi << 8 | lo )
+		op = '(' .. op .. ')'
+
+
+		return op, mem_pos + 2
+	end
+
+	if op == 'r8' then
+		local lo = memory:GetValue( mem_pos + 1 )
+		local minus = false
+		local origin = to_hex_string(lo)
+
+		if (lo & 0x80) == 0x80 then -- 반전 필요함. 그리고 음수임 =ㅁ =
+			lo = ( 0xff ~ lo ) + 1
+			minus = true
+		end
+
+		op = to_hex_string( lo )
+		if minus then
+			op = '-' .. op
+		end
+
+		return op, mem_pos + 1
+	end
+
 
 	if op == 'd8' or op == 'a8' then
 		local lo = memory:GetValue( mem_pos + 1 )
@@ -20,11 +49,22 @@ function get_op_translate_value( memory, mem_pos, op )
 	return op, nil
 end
 
-
-function Disassembler.render( self )
+function Disassembler.renderOpcodes( self, memory_address )
 	local memory = GetInstanceCPU():GetMemory()
+	local mem_pos = memory_address
+	local last_mem_pos = memory_address + 0x50
 
-	for mem_pos = 0x100, 0x110 do
+
+	for i = 1, 9999 do -- 대충 적음
+		if not ( mem_pos >= 0 and mem_pos <= ( 0xffff - 1 ) ) then
+			break
+		end
+
+		if mem_pos > last_mem_pos then
+			break
+		end
+
+
 		local value = memory:GetValue( mem_pos )
 		local tbl = self.vars.opcode_table['unprefixed']
 		local select_tbl = tbl[to_hex_string(value, nil, true)]
@@ -40,13 +80,17 @@ function Disassembler.render( self )
 			local op2 = select_tbl.operand2 or ''
 
 			if op1 == select_tbl.operand1 then
-				op1, next_mem_pos = get_op_translate_value( memory, mem_pos, op1 )
+				local pos = nil
+				op1, pos = get_op_translate_value( memory, mem_pos, op1 )
+				if pos then next_mem_pos = pos end
 
 				op1 = ' ' .. op1
 			end
 
 			if op2 == select_tbl.operand2 then
-				op2, next_mem_pos = get_op_translate_value( memory, mem_pos, op2 )
+				local pos = nil
+				op2, pos = get_op_translate_value( memory, mem_pos, op2 )
+				if pos then next_mem_pos = pos end
 
 				op2 = ', ' .. op2
 			end
@@ -57,14 +101,23 @@ function Disassembler.render( self )
 		local mem_pos_str = to_hex_string(mem_pos)
 		if next_mem_pos then
 			mem_pos_str = to_hex_string(mem_pos) .. '-' .. to_hex_string(next_mem_pos)
-			mem_pos = next_mem_pos - 1
+			mem_pos = next_mem_pos
 		end
 
 		text = mem_pos_str .. ': ' ..  to_hex_string(value) .. '/' .. mnemonic
-
 		ImGui.Text( text )
-	end
 
+		mem_pos = mem_pos + 1
+	end
+end
+
+function Disassembler.render( self )
+	ImGui.InputTextWithFlags( "",  self.vars.string_buf, ImGuiInputTextFlags_.CharsHexadecimal )
+	local memory_address = tonumber( self.vars.string_buf:GetViewString(), 16 )
+
+	if memory_address ~= nil and memory_address >= 0 and memory_address <= ( 0xffff - 1 ) then
+		self:renderOpcodes( memory_address )
+	end
 end
 
 function Disassembler.init( self )
@@ -79,4 +132,5 @@ function Disassembler.init( self )
 	local table = json.decode( text )
 
 	self.vars.opcode_table = table
+	self.vars.string_buf = StringBuf( 5 )
 end
