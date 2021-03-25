0c9c8058a205020b514e3aed94a7222e718f2c89
ffdd270 <ffdd270@gmail.com>
2021-03-25 23:35:48 +0900

    GEP-27 Gameboy - Disasmbler In Runtime
    파라미터가 상수면 해당 상수값으로 replace.

--- gameboy/script/disassembler.lua
+++ gameboy/script/disassembler.lua
@@ -1,9 +1,24 @@
 Disassembler ={}
 
-OPCODE_TO_COMMAND = {
+function get_op_translate_value( memory, mem_pos, op )
+	if op == 'a16' or op == 'd16' then
+		local hi = memory:GetValue( mem_pos + 2 )
+		local lo = memory:GetValue( mem_pos + 1 )
 
-}
+		op = to_hex_string(  hi << 8 | lo )
 
+		return op, mem_pos + 2
+	end
+
+
+	if op == 'd8' or op == 'a8' then
+		local lo = memory:GetValue( mem_pos + 1 )
+		op = to_hex_string( lo )
+		return op, mem_pos + 1
+	end
+
+	return op, nil
+end
 
 
 function Disassembler.render( self )
@@ -16,6 +31,7 @@ function Disassembler.render( self )
 
 		local text = ''
 		local mnemonic = ''
+		local next_mem_pos = nil
 
 		if select_tbl then
 			mnemonic = select_tbl.mnemonic
@@ -24,17 +40,27 @@ function Disassembler.render( self )
 			local op2 = select_tbl.operand2 or ''
 
 			if op1 == select_tbl.operand1 then
+				op1, next_mem_pos = get_op_translate_value( memory, mem_pos, op1 )
+
 				op1 = ' ' .. op1
 			end
 
 			if op2 == select_tbl.operand2 then
+				op2, next_mem_pos = get_op_translate_value( memory, mem_pos, op2 )
+
 				op2 = ', ' .. op2
 			end
 
 			mnemonic = mnemonic .. op1 .. op2
 		end
 
-		text = to_hex_string(mem_pos) .. ': ' ..  to_hex_string(value) .. '/' .. mnemonic
+		local mem_pos_str = to_hex_string(mem_pos)
+		if next_mem_pos then
+			mem_pos_str = to_hex_string(mem_pos) .. '-' .. to_hex_string(next_mem_pos)
+			mem_pos = next_mem_pos - 1
+		end
+
+		text = mem_pos_str .. ': ' ..  to_hex_string(value) .. '/' .. mnemonic
 
 		ImGui.Text( text )
 	end
